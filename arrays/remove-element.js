aecdf9235b5b64f720ee325cc3c179994e110e4c
dongct2504 <dongminhcho89@gmail.com>
2024-06-24 20:11:40 +0700

    chore: remove element

---
+++ arrays/remove-element.js
@@ -0,0 +1,27 @@
+/**
+ * 27. Remove Element
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+};
+
+const nums = [0, 1, 2, 2, 3, 0, 4, 2];
+const val = 2;
+
+console.log(removeElement(nums, val));
+console.log(nums);
